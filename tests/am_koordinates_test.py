1cf52551161343dd3e92efadaf68f65ca24de6f2
alejandramorawsp <alejandra.mora@wsp.com>
2024-02-22 15:18:23 +1100

    sec update

--- tests/am_koordinates_test.py
+++ tests/am_koordinates_test.py
@@ -1,7 +1,7 @@
 import koordinates
 
 client = koordinates.Client(
-    'wsp-au.koordinates.com', '1dc9b869d1894cc2a6700b54ab677380')
+    'wsp-au.koordinates.com', 'TOKEN')
 
 # for layer in client.layers.list():   print(layer.id)
 layer = client.layers.get(115992)
